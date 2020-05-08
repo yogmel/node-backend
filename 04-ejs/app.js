3a1b0f2de587f9ff58abb37ecb550bab8e06b005
Mellina Yonashiro <yonashiro.mellina@gmail.com>
2020-05-08 12:21:22 -0300

    Add EJS examples

---
+++ 04-ejs/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render('home.ejs');
+});
+
+app.get("/fallinginlovewith/:thing", (req, res) => {
+  const thing = req.params.thing
+  res.render('love.ejs', {thing});
+});
+
+app.get("/posts", (req, res) => {
+  const posts = [
+    {title: 'Post 1', author: 'lindona'},
+    {title: 'Post 2', author: 'Pepita'},
+    {title: 'Post 3', author: 'clark'},
+  ]
+  res.render('posts.ejs', {posts});
+});
+
+
+app.listen(3000, "localhost", () => {
+  console.log("Server started!");
+});
