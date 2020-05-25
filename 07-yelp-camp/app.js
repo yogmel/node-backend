0c5ca6ce01122cbfb10ff08a3bc9966def4358d7
Mellina Yonashiro <yonashiro.mellina@gmail.com>
2020-05-24 22:18:03 -0300

    Connect MongoDB with Yelp camp. Add mongoose example

--- 07-yelp-camp/app.js
+++ 07-yelp-camp/app.js
@@ -1,30 +1,52 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-app.use(express.static(__dirname + "/public"));
-const campgrounds = [
-  {name: "Salmon Creek", image: "http://blog.tremeterra.com.br/wp-content/uploads/2017/10/scott-goodwill-359336-1-1-700x510.jpg"},
-  {name: "Granite Hill", image: "http://blog.tremeterra.com.br/wp-content/uploads/2017/10/scott-goodwill-359336-1-1-700x510.jpg"},
-  {name: "Mountain Goat's Rest", image: "http://blog.tremeterra.com.br/wp-content/uploads/2017/10/scott-goodwill-359336-1-1-700x510.jpg"},
-]
+const express = require('express'),
+      app = express(),
+      bodyParser = require('body-parser'),
+      mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost:27017/yelp_camp", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+// SCHEMA SETUP
+const campgroundsSchema = new mongoose.Schema({
+  name: String,
+  image: String
+})
+
+const Campground = mongoose.model("Campground", campgroundsSchema);
 
 app.get('/', (req, res) => {
   res.render('landing');
 })
 
 app.get('/campgrounds', (req, res) => {
-  res.render('campgrounds', { campgrounds });
+  Campground.find({}, (err, campgrounds) => {
+    try {
+      res.render('campgrounds', { campgrounds });
+    } catch(err) {
+      console.log(err);
+    }
+  })
 })
 
 app.post('/campgrounds', (req, res) => {
   const name = req.body.name;
   const image = req.body.imgurl;
 
-  campgrounds.push({name, image});
-  res.redirect('/campgrounds');
+  Campground.create({name, image}, (err, campground) => {
+    try {
+      console.log("NEWLY CREATED CAMPGROUND:");
+      console.log(campground);
+      res.redirect('/campgrounds');
+    } catch {
+      console.log(err);
+    }
+  });
 })
 
 app.get('/campgrounds/new', (req, res) => {
