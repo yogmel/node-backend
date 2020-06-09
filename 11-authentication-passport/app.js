385debd139bb6282989df2ecb583163b361518ac
Mellina Yonashiro <yonashiro.mellina@gmail.com>
2020-06-09 11:06:29 -0300

    Add register, login and logout routes and logic

--- 11-authentication-passport/app.js
+++ 11-authentication-passport/app.js
@@ -3,8 +3,7 @@ const express = require("express"),
   passport = require("passport"),
   bodyParser = require("body-parser"),
   User = require("./models/user"),
-  LocalStrategy = require("passport-local"),
-  passportLocalMongoose = require("passport-local-mongoose");
+  LocalStrategy = require("passport-local");
 
 mongoose.connect("mongodb://localhost:27017/auth_demo_app", {
   useNewUrlParser: true,
@@ -13,6 +12,7 @@ mongoose.connect("mongodb://localhost:27017/auth_demo_app", {
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   require("express-session")({
     secret: "Rusty is the best and cutest dog in the world",
@@ -24,15 +24,59 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+const isLoggedin = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/register");
+};
 
 app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.get("/secret", (req, res) => {
+app.get("/secret", isLoggedin, (req, res) => {
   res.render("secret");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", (req, res) => {
+  const { username, password } = req.body;
+  User.register(new User({ username }), password)
+    .then((user) => {
+      console.log(user);
+      passport.authenticate("local")(req, res, () => {
+        res.redirect("/secret");
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.render("register");
+    });
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/login",
+  })
+);
+
+app.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
 });
 
 app.listen(3000, "localhost", () => {
